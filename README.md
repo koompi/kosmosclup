cf0cdaf82889424f3b82ea11d6f903350f0de41a
LyhourChhen <lyhourchhen@LyhourChhens-MacBook-Pro.local>
2018-11-07 17:27:37 +0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Koompifixscriptfile
+# kosmosclup
+# KOSMOS Clearnup
+ A script that cleanup old apps and libs on the KOSMOS 3.6.9 LTS for KOOMPI Notebook 2018
+ ##HOW TO USE ?
+You can download kosmosclup by cloning the Git Repo and simply installing its requirements
+```sh
+~ ❯❯❯ git clone https://github.com/Koompi/kosmosclup.git
+~ ❯❯❯ cd kosmosclup
+~ ❯❯❯ sh kosmosclup.sh
+```
+#Thank you for using KOOPI.
+
---
+++ kosmosclup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#Author : Koompi Team
+#Version......: 1.00
+cat << EOF 
+   _  ______   ____  __  __ _____ _____   _  ______   _____ __  __  ____   _____ 
+ | |/ / __ \ / __ \|  \/  |  __ \_   _| | |/ / __ \ / ____|  \/  |/ __ \ / ____|
+ | ' / |  | | |  | | \  / | |__) || |   | ' / |  | | (___ | \  / | |  | | (___  
+ |  <| |  | | |  | | |\/| |  ___/ | |   |  <| |  | |\___ \| |\/| | |  | |\___ \ 
+ | . \ |__| | |__| | |  | | |    _| |_  | . \ |__| |____) | |  | | |__| |____) |
+ |_|\_\____/ \____/|_|  |_|_|   |_____| |_|\_\____/|_____/|_|  |_|\____/|_____/ 
+                                                                                
+                                                    CLEAN UP  v1.0                            
+                                                                                                                                                          
+EOF
+                                                                    
+
+#Add git to system
+sudo apt-get install git -y
+#Remove wps-office from system
+sudo apt remove wps-office -y
+sudo apt autoremove -y
+#Install LibraOfiice
+sudo add-apt-repository ppa:libreoffice/ppa -y
+sudo apt update -y
+sudo apt install libreoffice -y
+
+#move libraaccount-glib
+sudo rsync -avz ./Resourse/libaccounts-glib/* /home/koompi/.config/libaccounts-glib/
+#move paper
+sudo rsync -avz ./Resourse/Paper/* /usr/share/icons/Paper/
+#move ttf-khmeros
+sudo rsync -avz ./Resourse/ttf-khmeros/* /usr/share/fonts/truetype/ttf-khmeros/
+cd ..
+echo "Deleting file...."
+rm -rf kosmosclup
+
+echo "Done!!!"
+echo "Thank you for using KOOMPI"
