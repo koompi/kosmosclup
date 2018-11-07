bb34f19f53d853b477a04e799dd0185363ad9d02
LyhourChhen <lyhourchhen@LyhourChhens-MacBook-Pro.local>
2018-11-07 18:15:00 +0700

    Update Icon

--- kosmosclup.sh
+++ kosmosclup.sh
@@ -25,11 +25,11 @@ sudo apt update -y
 sudo apt install libreoffice -y
 
 #move libraaccount-glib
-sudo rsync -avz ./Resourse/libaccounts-glib/* /home/koompi/.config/libaccounts-glib/
+sudo rsync -avz ./resources/libaccounts-glib/* /home/koompi/.config/libaccounts-glib/
 #move paper
-sudo rsync -avz ./Resourse/Paper/* /usr/share/icons/Paper/
+sudo rsync -avz ./resources/Paper/* /usr/share/icons/Paper/
 #move ttf-khmeros
-sudo rsync -avz ./Resourse/ttf-khmeros/* /usr/share/fonts/truetype/ttf-khmeros/
+sudo rsync -avz ./resourses/ttf-khmeros/* /usr/share/fonts/truetype/ttf-khmeros/
 cd ..
 echo "Deleting file...."
 rm -rf kosmosclup
